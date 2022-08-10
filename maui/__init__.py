b08328b44120518a9c805a170ddcceb33415e269
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2022-08-10 12:40:17 -0300

    first commit. create io module

---
+++ maui/__init__.py
@@ -0,0 +1,14 @@
+import librosa
+import librosa.display
+import audioread
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import os
+import glob
+
+import time
+import datetime
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='maui',
+      version='0.1',
+      description='Eco-acoustics data visualization and analysis',
+      url='---',
+      author='Caio Ferreira',
+      author_email='caio.bernardo@usp.br',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'librosa',
+          'audioread',
+          'numpy',
+          'pandas',
+          'matplotlib'
+      ],
+      zip_safe=False)
