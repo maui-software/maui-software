6e2610f60da66d859433781a4f42ae5c2b36db33
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2024-04-05 08:55:48 -0300

    fix samples method and file

--- maui/samples/__init__.py
+++ maui/samples/__init__.py
@@ -6,12 +6,12 @@ The module ``samples`` has a collection of data samples to be used as examples.
 .. autosummary::
     :toctree: generated/
 
-    get_leec_audio_sample
+    get_audio_sample
 
 """
 
-from .audio_samples import (get_leec_audio_sample)
+from .audio_samples import (get_audio_sample)
 
 
 __all__ = [ # visualizations
-           'get_leec_audio_sample']
+           'get_audio_sample']
